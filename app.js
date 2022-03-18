af8d21da6fef1f3b531ca241a35c7a518bda9101
buiducthinh200820 <101413818+buiducthinh200820@users.noreply.github.com>
2022-03-18 22:42:26 +0700

    first commit

--- app.js
+++ app.js
@@ -33,6 +33,14 @@ const musics = [
     image:
       "https://images.unsplash.com/photo-1606800052052-a08af7148866?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
   },
+  {
+    id: 4,
+    title: "Unstopable",
+    file: "unstopable.mp3",
+    image:
+      "https://images.unsplash.com/photo-1512310604669-443f26c35f52?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=734&q=80",
+  },
+  
 
 ];
 
