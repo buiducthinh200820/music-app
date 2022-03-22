824a5efa00f068b06610bb5f927ef98be69dfb7e
buiducthinh200820 <101413818+buiducthinh200820@users.noreply.github.com>
2022-03-22 16:40:31 +0700

    first commit

--- app.js
+++ app.js
@@ -8,9 +8,38 @@ const rangeBar = document.querySelector(".range");
 const musicName = document.querySelector(".music-name");
 const musicThumbnail = document.querySelector(".music-thumb");
 const musicImage = document.querySelector(".music-thumb img");
+const playRepeat=document.querySelector(".play-repeat");
+const volume=document.querySelector(".volume");
+
+playRepeat.addEventListener("click",function(){
+  if(isRepeat) 
+  {
+    isRepeat =false;
+    playRepeat.removeAttribute("style")
+  }
+  else
+    { 
+      isRepeat =true;
+      playRepeat.style.color ="#ffb86c"
+    }
+});
+
 
 let isPlaying = true;
 let indexSong = 0;
+let isRepeat =false;
+let timer;
+let isVolume =0;
+volume.addEventListener("click",function(){
+  if (isVolume) {
+    volume.innerHTML = `<ion-icon name="pause-circle"></ion-icon>`;
+    isPlaying = 0;
+  } else {
+    volume.innerHTML = `<ion-icon name="play"></ion-icon>`;
+    isPlaying = 1;
+  }
+});
+
 const musics = [
   {
     id: 1,
@@ -40,6 +69,13 @@ const musics = [
     image:
       "https://images.unsplash.com/photo-1512310604669-443f26c35f52?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=734&q=80",
   },
+  {
+    id: 5,
+    title: "waiting for love ",
+    file: "waitingforlove.mp3",
+    image:
+      "https://images.unsplash.com/photo-1516980907201-943c13a8d03c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+  },
   
 
 ];
@@ -51,8 +87,6 @@ const musics = [
  * file: holo.mp3
  * image: unsplash
  */
-let timer;
-
 nextBtn.addEventListener("click", function () {
   changeSong(1);
 });
@@ -83,7 +117,13 @@ function changeSong(dir) {
 //chạy bài tiếp theo 
 song.addEventListener("ended",handEnderSong);
 function handEnderSong(){
-  changeSong(1);
+  if(isRepeat){
+    isPlaying=true;
+    playPause();
+  }else{
+    changeSong(1);
+  }
+  
 }
 
 playBtn.addEventListener("click", playPause);
